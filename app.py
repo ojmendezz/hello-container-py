661cff79d274205da5d23229d716c31f345b996d
Oscar Mendez <ojmendezz@gmail.com>
2022-01-17 22:34:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, from inside the original application!"
+    return "Hello Cloud Run with Github integration!"
 
 
 if __name__ == "__main__":
